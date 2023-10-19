b651dfe96e2841d4597c425e91d0416a7851dd92
rakibul Islam <irakibul026@gmail.com>
2023-10-19 13:34:55 +0530

    remove inter Font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
 	title: "iFerry- Rakib",
@@ -12,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>
+			<body>
 				{children}
 				<ToastContainer position='bottom-right' autoClose={1200} theme='dark' />
 			</body>
